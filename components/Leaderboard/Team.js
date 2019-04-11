97462b9c9431c399bced14c64815cbaf661133b3
Alex Ray <alex.ray12@gmail.com>
2019-04-11 09:51:08 -0700

    Migrate Team tables to material-ui

--- components/Leaderboard/Team.js
+++ components/Leaderboard/Team.js
@@ -1,46 +1,62 @@
 import _ from 'lodash';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const Team = ({ name, team }) => {
   const missedCutBonus =
     team.missedCuts === 0 ? -10 : team.missedCuts === 1 ? -5 : 0;
   return (
     <div>
-      <h2>{name}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Player</th>
-            <th>Pos</th>
-            <th>Bonus</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Typography variant="h4" align="center" gutterBottom>
+        {name}
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Player</TableCell>
+            <TableCell align="right">Pos</TableCell>
+            <TableCell align="right">Bonus</TableCell>
+            <TableCell align="right">Total</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {_.get(team, 'players', []).map(player => (
-            <tr>
-              <td>
+            <TableRow>
+              <TableCell>
                 {player.firstName} {player.lastName}
-              </td>
-              <td>{player.position}</td>
-              <td>{player.bonus}</td>
-            </tr>
+              </TableCell>
+              <TableCell align="right">{player.position}</TableCell>
+              <TableCell align="right">{player.bonus}</TableCell>
+              <TableCell align="right">
+                {player.rawScore + player.bonus}
+              </TableCell>
+            </TableRow>
           ))}
-          <tr>
-            <td colSpan="2">Missed Cut Bonus</td>
-            <td>{missedCutBonus}</td>
-          </tr>
-          <tr>
-            <td colSpan="2">
+          <TableRow>
+            <TableCell rowSpan="2" colSpan="2" />
+            <TableCell>Missed Cut Bonus</TableCell>
+            <TableCell align="right">{missedCutBonus}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
               <strong>Total</strong>
-            </td>
-            <td>
-              {_.get(team, 'players', []).reduce(
-                (accum, player) => accum + player.rawScore + player.bonus,
-                missedCutBonus
-              )}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </TableCell>
+            <TableCell align="right">
+              <strong>
+                {_.get(team, 'players', []).reduce(
+                  (accum, player) => accum + player.rawScore + player.bonus,
+                  missedCutBonus
+                )}
+              </strong>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
