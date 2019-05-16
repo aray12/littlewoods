9118d72b3bb5481f96444e3fcbd16d76a4db99a0
Alex Ray <alex.ray12@gmail.com>
2019-05-16 15:28:45 -0700

    Add encoding dependency to function

--- src/functions/fetchLeaderboard/fetchLeaderboard.js
+++ src/functions/fetchLeaderboard/fetchLeaderboard.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch("https://lbdata.pgatour.com/2019/r/033/leaderboard.json", {
-      headers: { Accept: "application/json" }
-    });
+    const response = await fetch(
+      'https://lbdata.pgatour.com/2019/r/033/leaderboard.json',
+      {
+        headers: { Accept: 'application/json' },
+      }
+    );
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText };
@@ -14,13 +17,13 @@ exports.handler = async function(event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     };
   } catch (err) {
     console.log(err); // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
     };
   }
 };
